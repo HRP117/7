ac5aaf4fb122d5f71da9125df7895e62822b25c2
HRP117 <72773916+HRP117@users.noreply.github.com>
2024-08-22 00:07:51 -0600

    Sumador completo de 1 bit

---
+++ FullAdder/full_adder_tb.vhdl
@@ -0,0 +1,45 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity FullAdderTestBench is
+end FullAdderTestBench;
+
+architecture Structural of FullAdderTestBench is
+    component FullAdder
+        port (
+            cin: in std_logic;
+            x: in std_logic;
+            y: in std_logic;
+            sigma: out std_logic;
+            cout: out std_logic
+        );
+    end component;
+    
+    signal CIN:     std_logic;
+    signal X:       std_logic;
+    signal Y:       std_logic;
+    signal SIGMA:   std_logic;
+    signal COUT:    std_logic;
+    
+begin
+    FA: FullAdder port map (
+        cin => CIN,
+        x => X,
+        y => Y,
+        sigma => SIGMA,
+        cout => COUT
+    );
+    
+    stimuli: process
+    begin
+        CIN <= '0'; X <= '0'; Y <= '0'; wait for 1 ns;
+        CIN <= '0'; X <= '0'; Y <= '1'; wait for 1 ns;
+        CIN <= '0'; X <= '1'; Y <= '0'; wait for 1 ns;
+        CIN <= '0'; X <= '1'; Y <= '1'; wait for 1 ns;
+        CIN <= '1'; X <= '0'; Y <= '0'; wait for 1 ns;
+        CIN <= '1'; X <= '0'; Y <= '1'; wait for 1 ns;
+        CIN <= '1'; X <= '1'; Y <= '0'; wait for 1 ns;
+        CIN <= '1'; X <= '1'; Y <= '1'; wait for 1 ns;
+        wait;
+    end process stimuli;
+end Structural;
