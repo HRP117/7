67115697df5b98d9dc3ea49ea0b080dae10d8f5c
HRP117 <72773916+HRP117@users.noreply.github.com>
2024-08-27 09:39:48 -0600

    ALU

---
+++ ALU/alu.vhdl
@@ -0,0 +1,64 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.math_real.all;
+
+entity ALU is
+    generic (
+        N : integer
+    );
+    port (
+        S: in std_logic_vector(2 downto 0); 
+        A: in std_logic_vector(N-1 downto 0); 
+        B: in std_logic_vector(N-1 downto 0);
+        F: out std_logic_vector(N-1 downto 0);
+        UOF: out std_logic;
+        SOF: out std_logic
+    );
+end entity ALU;
+
+architecture Structural of ALU is
+    
+    component FullAdder
+            port (
+            cin: in std_logic;
+            x: in std_logic;
+            y: in std_logic;
+            sigma: out std_logic;
+            cout: out std_logic
+        );
+    end component;
+
+    signal x, y : std_logic_vector(N-1 downto 0); -- Señales internas
+    signal c: std_logic_vector(N downto 0); -- Acarreo interno
+
+begin
+    c(0) <= s(2) and (s(1) xor s(0));
+    
+    LEGEN: for i in 0 to N-1 generate
+        x(i) <= (S(2) and A(i)) or (not(S(0)) and A(i)) or 
+        (not(S(1)) and A(i) and B(i)) or 
+        (not(S(2)) and S(1) and not(A(i)) 
+        and (B(i) or S(0)));
+    end generate LEGEN;
+    
+    AEGEN: for i in 0 to N-1 generate
+        y(i) <= (S(2) and S(0) and (S(1) or not(B(i)))) or 
+        (S(2) and not(S(1)) and not(S(0)) and B(i));
+    end generate AEGEN;
+    
+    ADDGEN: for i in 0 to N-1 generate
+        FA: entity FullAdder
+            port map (
+                x => x(i),
+                y => y(i),
+                cin => c(i),
+                sigma => F(i),
+                cout => c(i+1)
+            );
+    end generate ADDGEN;
+
+    UOF <= c(N);
+    SOF <= c(N) xor c(N-1);
+
+
+end Structural;
---
+++ ALU/alu_tb.vhdl
@@ -0,0 +1,90 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ALU_tb is
+    generic (
+        N : integer := 4
+    );
+
+end entity ALU_tb;
+
+architecture Structural of ALU_tb is
+
+    signal S    : std_logic_vector(2 downto 0);
+    signal A    : std_logic_vector(N-1 downto 0);
+    signal B    : std_logic_vector(N-1 downto 0);
+    signal F    : std_logic_vector(N-1 downto 0);
+    signal UOF  : std_logic;
+    signal SOF  : std_logic;
+
+    component ALU
+        generic (
+            N : integer
+        );
+        port (
+            S: in std_logic_vector(2 downto 0); 
+            A: in std_logic_vector(N-1 downto 0); 
+            B: in std_logic_vector(N-1 downto 0);
+            F: out std_logic_vector(N-1 downto 0);
+            UOF: out std_logic;
+            SOF: out std_logic
+        );
+    end component;
+    
+begin
+
+    ALU: ALU
+        generic map (
+            N => N
+        )
+        port map (
+            S => S,
+            A => A,
+            B => B,
+            F => F,
+            UOF => UOF,
+            SOF => SOF
+        );
+
+    stimuli: process
+    begin
+        A <= (others=>'1');
+        B <= (others=>'0');
+
+        -- Caso 000: Pasar A a F
+        S <= "000";
+        wait for 10 ns;
+
+        -- Caso 001: AND entre A y B
+        S <= "001";
+        wait for 10 ns;
+
+        -- Caso 002: OR entre A y B
+        S <= "010";
+        wait for 10 ns;
+
+        -- Caso 011: NOT A
+        S <= "011";
+        wait for 10 ns;
+
+        -- Caso 100: A + B
+        S <= "100";
+        wait for 10 ns;
+
+        -- Caso 101: A - B
+        S <= "101";
+        wait for 10 ns;
+
+        -- Caso 110: A + 1 
+        S <= "110";
+        wait for 10 ns;
+
+        -- Caso 111: A - 1 
+        S <= "111";
+        wait for 10 ns;
+
+        wait;
+    end process;
+
+end Structural;
