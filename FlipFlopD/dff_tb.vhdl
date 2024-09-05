7a16b88eb10b94e46cd7ac9fcd14304b45a72e18
HRP117 <72773916+HRP117@users.noreply.github.com>
2024-09-05 09:38:47 -0600

    TEST BENCH FLIP FLOP TIPO D

--- FlipFlopD/dff_tb.vhdl
+++ FlipFlopD/dff_tb.vhdl
@@ -2,33 +2,62 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity DFF is
-    port(
-        D:      in std_logic;       --Data input
-        E:      in std_logic;       --Enable
-        R:      in std_logic;       --Reset
-        P:      in std_logic;       --Preset
-        clk:    in std_logic;       --Clock
-        Q:      out std_logic       --State
-    );
-    
-end entity DFF;
+entity DFF_tb is
+end entity DFF_tb;
 
-architecture Behavioral of DFF is
+architecture Structural of DFF_tb is
+    component DFF
+        port(
+            D:      in std_logic;       --Data input
+            E:      in std_logic;       --Enable
+            R:      in std_logic;       --Reset
+            P:      in std_logic;       --Preset
+            clk:    in std_logic;       --Clock
+            Q:      out std_logic       --State
+        );
+    end component;
+
+    signal D  : std_logic;
+    signal E  : std_logic;
+    signal R  : std_logic;
+    signal P  : std_logic;
+    signal CLK  : std_logic;
+    signal Q  : std_logic;
+    signal finish_flag  :   boolean :=   false;  --Stop process
 
 begin
-    behavior: process (clk, R, P)
-    begin
-        if (R'event and R = '1') then
-            Q <= '0';
-        elsif (P'event and R = '1') then
-            Q <= '1';
-        elsif (clk'event and R = '1' and E= '1') then
-            Q <= D;
-        end if;
-    end process behavior;
+    DFF_GEN: DFF
+        port map(
+            D => D,
+            E => E,
+            R => R,
+            P => P,
+            clk => CLK,
+            Q => Q
+        );
 
-end Behavioral;
+    --Each milisecond the clock signal is complemented
+    --This process runs indefinitely
+    CLK_GEN: process begin
+        wait for 1 ms;
+        CLK <= not CLK;
+        if (finish_flag = true) then
+            wait;
+        end if ;
+    end process CLK_GEN;
+
+    --This process runs at the same time as CLK_GEN
+    stimuli: process begin
+        --Asynchronous evaluation
+        R <= '0'; P <= '0';     wait for 0.7 ms;        -- Q = X
+        R <= '1';               wait for 1.5 ms;        -- Q = '0'
+        P <= '1';               wait for 2.3 ms;        -- Q = '1'
+        R <= '0'; P <= '0';     wait for 0.7 ms;        -- Q = '1'
+        R <= '1'; P <= '1';     wait for 0.7 ms;        -- Q = '0'
+        finish_flag <= true;
+    end process stimuli;
+
+end Structural;
 
 --▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 --▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
